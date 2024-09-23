2563c15b138139a563265a82d8ba35c9972431a4
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-23 16:37:39 +0530

    Classes,Objects

--- Python-Assesments/main.py
+++ Python-Assesments/main.py
@@ -96,18 +96,64 @@
 # else:
 #   print("Finally finished!")
 
-#Array
-cars = ["Ford", "Volvo", "BMW"]
-x = cars[0]
+# #Array
+# cars = ["Ford", "Volvo", "BMW"]
+# x = cars[0]
+#
+# x = len(cars)
+#
+# for x in cars:
+#   print(x)
+#
+# cars.append("Honda")
+#
+# cars.pop(1)
+#
+# cars.remove("Volvo")
 
-x = len(cars)
+#Classes , Objects
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-for x in cars:
-  print(x)
+p1 = Person("John", 36)
 
-cars.append("Honda")
+print(p1)
 
-cars.pop(1)
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-cars.remove("Volvo")
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
 
