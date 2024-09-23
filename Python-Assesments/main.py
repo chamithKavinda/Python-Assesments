b7b217f684542b0cfddf45ab177b927e5180cf48
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-23 16:35:05 +0530

    Array

--- Python-Assesments/main.py
+++ Python-Assesments/main.py
@@ -1,99 +1,113 @@
-#This is a comment
-print("Hello, World!")
+# #This is a comment
+# print("Hello, World!")
+#
+# #Variables
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+#
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+#
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+#
+# #Global Variable
+# x = "awesome"
+#
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+#
+# myfunc()
+#
+# print("Python is " + x)
+#
+# #Type Conversion
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+#
+# #convert from int to float:
+# a = float(x)
+#
+# #convert from float to int:
+# b = int(y)
+#
+# #convert from int to complex:
+# c = complex(x)
+#
+# print(a)
+# print(b)
+# print(c)
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+#
+# #if-else condition
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+#
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a == b else print("B")
+#
+# a = 200
+# b = 33
+# c = 500
+# if a > b or a > c:
+#   print("At least one of the conditions is True")
+#
+# x = 41
+#
+# if x > 10:
+#   print("Above ten,")
+#   if x > 20:
+#     print("and also above 20!")
+#   else:
+#     print("but not above 20.")
+#
+#
+# #for loop
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     break
+#   print(x)
+#
+# for x in range(6):
+#   print(x)
+# else:
+#   print("Finally finished!")
 
-#Variables
-x = 5
-y = "John"
-print(x)
-print(y)
+#Array
+cars = ["Ford", "Volvo", "BMW"]
+x = cars[0]
 
-x, y, z = "Orange", "Banana", "Cherry"
-print(x)
-print(y)
-print(z)
+x = len(cars)
 
-x = "Python"
-y = "is"
-z = "awesome"
-print(x, y, z)
-
-#Global Variable
-x = "awesome"
-
-def myfunc():
-  x = "fantastic"
-  print("Python is " + x)
-
-myfunc()
-
-print("Python is " + x)
-
-#Type Conversion
-x = 1    # int
-y = 2.8  # float
-z = 1j   # complex
-
-#convert from int to float:
-a = float(x)
-
-#convert from float to int:
-b = int(y)
-
-#convert from int to complex:
-c = complex(x)
-
-print(a)
-print(b)
-print(c)
-
-print(type(a))
-print(type(b))
-print(type(c))
-
-#if-else condition
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
-
-a = 330
-b = 330
-print("A") if a > b else print("=") if a == b else print("B")
-
-a = 200
-b = 33
-c = 500
-if a > b or a > c:
-  print("At least one of the conditions is True")
-
-x = 41
-
-if x > 10:
-  print("Above ten,")
-  if x > 20:
-    print("and also above 20!")
-  else:
-    print("but not above 20.")
-
-
-#for loop
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
+for x in cars:
   print(x)
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  if x == "banana":
-    break
-  print(x)
+cars.append("Honda")
 
-for x in range(6):
-  print(x)
-else:
-  print("Finally finished!")
+cars.pop(1)
 
+cars.remove("Volvo")
 
